a450459fc1de0357c4579e2577365dcef98abd8c
zabik <zaaabik@gmail.com>
2018-12-07 05:12:40 +0300

    change fit --> fit_generator (generator do not load all data to ram at one time)

---
+++ metric_learning/generator.py
@@ -0,0 +1,30 @@
+import numpy as np
+from skimage.io import imread
+from skimage.transform import resize
+from tensorflow import keras
+from tensorflow.python.keras.utils import Sequence
+
+
+class Generator(Sequence):
+    def __init__(self, all_files, all_labels, image_size, batch_size, classes_count):
+        self.batch_size = batch_size
+        self.image_size = image_size
+        self.all_files = all_files
+        self.all_labels = all_labels
+        self.classes_count = classes_count
+
+    def __len__(self):
+        return len(self.all_files) // self.batch_size
+
+    def __getitem__(self, idx):
+        batch_x = self.all_files[idx * self.batch_size:(idx + 1) * self.batch_size]
+        batch_y = self.all_labels[idx * self.batch_size:(idx + 1) * self.batch_size]
+
+        y_labels = keras.utils.to_categorical(np.array(batch_y), self.classes_count)
+        images = np.array([
+            resize(imread(file_name), (self.image_size, self.image_size))
+            for file_name in batch_x])
+
+        dummy = np.zeros((np.array(batch_x).shape[0], 1))
+
+        return [images, y_labels], [y_labels, dummy]
--- metric_learning/main.py
+++ metric_learning/main.py
@@ -1,19 +1,14 @@
 import optparse
 import os
-import random
 
-import cv2
-import numpy as np
-from matplotlib import pyplot as plt
-from tensorflow import keras
 from tensorflow.keras import Model
 from tensorflow.keras import backend as K
 from tensorflow.keras.layers import Layer, Input
 from tensorflow.python.keras import optimizers, losses
-from tensorflow.python.keras.layers import Conv2D, MaxPool2D, Dense, BatchNormalization, Activation, \
+from tensorflow.python.keras.layers import Conv2D, Dense, Activation, \
     GlobalAveragePooling2D
 
-from metric_learning.resnet34 import level0, level1, level2, level3, level4
+from metric_learning.generator import Generator
 
 class_name_max = 10
 output_len = 128
@@ -28,131 +23,17 @@ parser.add_option('--batch')
 parser.add_option('--epochs')
 (options, args) = parser.parse_args()
 
-
 batch_size = int(options.batch)
 lr = float(options.lr)
 center_weight = float(options.center)
 epochs = int(options.epochs)
 class_name_max = int(options.classes)
 
-class TripletLossLayer(Layer):
-    def __init__(self, alpha=0.2, **kwargs):
-        self.alpha = alpha
-        super(TripletLossLayer, self).__init__(**kwargs)
-
-    def triplet_loss(self, inputs):
-        threshold = 0.6
-        margin = 0.12
-        a, p, n = inputs
-        p_dist = K.sqrt(K.sum(K.square(a - p), axis=1))
-        n_dist = K.sqrt(K.sum(K.square(a - n), axis=1))
-        p_loss = K.clip(p_dist - threshold + margin, 0, np.inf)
-        n_loss = K.clip(-n_dist + threshold + margin, 0, np.inf)
-        return K.mean(p_loss) + K.mean(n_loss)
-
-    def call(self, inputs):
-        loss = self.triplet_loss(inputs)
-        self.add_loss(loss)
-        return loss
-
-
-def scheduler(epoch):
-    lr = 0.1
-    return lr / (((epoch // 100) + 1) * 10)
-
-
-def get_data(path=r'C:\datasets\vgg2'):
-    folders = os.listdir(path)
-    data = []
-    short_labels = []
-    class_name = 0
-    for folder in folders:
-        cur = path + os.path.sep + folder
-        files = os.listdir(cur)
-        for file in files:
-            file = cur + os.path.sep + file
-            img = cv2.imread(file)
-            img = cv2.resize(img, (input_image_size, input_image_size))
-            short_labels.append(class_name)
-            img = np.array(img)
-            img = img.astype('float32')
-            img /= 256
-            data.append(img)
-        class_name = class_name + 1
-        if class_name >= class_name_max:
-            break
-    return np.array(data), np.array(short_labels)
-
-
-def create_triplet(data, labels):
-    labels = np.array(labels)
-    random.shuffle(labels)
-    data = np.array(data)
-    random.shuffle(data)
-    a = []
-    p = []
-    n = []
-
-    for d, l in zip(data, labels):
-        a.append(d)
-        idxs = np.argwhere(labels != l)
-        idx = random.choice(idxs)
-        n.append(data[idx][0])
-        idxs = np.argwhere(labels == l)
-        idx = random.choice(idxs)
-        p.append(data[idx][0])
-    return np.array(a), np.array(p), np.array(n)
-
-
-def create_triplet_generator(data, labels, batch_size):
-    while True:
-        labels = np.array(labels)
-        random.shuffle(labels)
-        data = np.array(data)
-        random.shuffle(data)
-        a = []
-        p = []
-        n = []
-
-        i = 0
-        for d, l in zip(data, labels):
-            if i >= batch_size:
-                break
-            i += 1
-            a.append(d)
-            idxs = np.argwhere(labels != l)
-            idx = random.choice(idxs)
-            n.append(data[idx][0])
-            idxs = np.argwhere(labels == l)
-            idx = random.choice(idxs)
-            p.append(data[idx][0])
-        features = [np.array(a), np.array(p), np.array(n)]
-
-        yield features, None
-
-
-def get_test_image(path):
-    img = cv2.imread(path)
-    img = cv2.resize(img, (input_image_size, input_image_size))
-    img = np.array(img)
-    img = img.astype('float32')
-    img /= 256
-    img = np.expand_dims(img, axis=0)
-    return img
-
 
 def create_resnet():
     image_input = Input(shape=(input_image_size, input_image_size, 3))
     prev = Conv2D(37, (7, 7), (2, 2))(image_input)
     prev = Activation('relu')(prev)
-    prev = BatchNormalization()(prev)
-    prev = MaxPool2D(pool_size=(3, 3), strides=(2, 2))(prev)
-
-    prev = level4(prev)
-    prev = level3(prev)
-    prev = level2(prev)
-    prev = level1(prev)
-    prev = level0(prev)
     prev = GlobalAveragePooling2D()(prev)
     prev = Dense(output_len, use_bias=False)(prev)
     return Model(image_input, prev)
@@ -166,21 +47,20 @@ class CenterLossLayer(Layer):
 
     def build(self, input_shape):
         self.centers = self.add_weight(name='centers',
-                                       shape=(class_name_max, 128),
+                                       shape=(class_name_max, output_len),
                                        initializer='uniform',
                                        trainable=False)
         super().build(input_shape)
 
     def call(self, x, mask=None):
-        # x[0] is Nx2, x[1] is Nx10 onehot, self.centers is 10x2
-        delta_centers = K.dot(K.transpose(x[1]), (K.dot(x[1], self.centers) - x[0]))  # 10x2
-        center_counts = K.sum(K.transpose(x[1]), axis=1, keepdims=True) + 1  # 10x1
+        delta_centers = K.dot(K.transpose(x[1]), (K.dot(x[1], self.centers) - x[0]))
+        center_counts = K.sum(K.transpose(x[1]), axis=1, keepdims=True) + 1
         delta_centers /= center_counts
         new_centers = self.centers - self.alpha * delta_centers
         self.add_update((self.centers, new_centers), x)
         self.result = x[0] - K.dot(x[1], self.centers)
-        self.result = K.sum(self.result ** 2, axis=1, keepdims=True)  # / K.dot(x[1], center_counts)
-        return self.result  # Nx1
+        self.result = K.sum(self.result ** 2, axis=1, keepdims=True)
+        return self.result
 
     def compute_output_shape(self, input_shape):
         return K.int_shape(self.result)
@@ -197,69 +77,39 @@ def train_resnet():
     side = CenterLossLayer(alpha=0.5, name='centerlosslayer')([resnet.output, aux_input])
 
     model = Model(inputs=[resnet.input, aux_input], outputs=[main, side])
-    optim = optimizers.SGD(lr=1e-3, momentum=0.9)
+    optim = optimizers.SGD(lr=lr, momentum=0.9)
     model.compile(optimizer=optim,
                   loss=[losses.categorical_crossentropy, zero_loss],
-                  loss_weights=[1, center_weight])
-    f, l = get_data(options.dataset)
-    labels_one_hot = keras.utils.to_categorical(l, class_name_max)
-    dummy = np.zeros((f.shape[0], 1))
-    model.fit([f, labels_one_hot], [labels_one_hot, dummy], epochs=epochs, verbose=2, batch_size=batch_size)
+                  loss_weights=[1, center_weight], metrics=['accuracy'])
+    all_files, all_labels = get_files(options.dataset)
+    dataset_len = len(all_files)
+    model.fit_generator(Generator(all_files, all_labels, input_image_size, batch_size, class_name_max),
+                        epochs=epochs,
+                        steps_per_epoch=dataset_len // batch_size
+                        )
     model.save_weights('resnet2d.h5')
 
 
-def test_reznet():
-    res_net = create_resnet()
+def get_files(path):
+    files_count = 0
+    all_files = []
+    all_labels = []
+    folders = os.listdir(path)
+    for current_label, folder in enumerate(folders):
+        cur = path + os.path.sep + folder
+        files = os.listdir(cur)
+        for idx, val in enumerate(files):
+            files[idx] = cur + os.path.sep + files[idx]
+        current_folder_files_count = len(files)
 
-    a = get_test_image(r'C:\datasets\vgg2\n000149\0003_01.jpg')
-    p = get_test_image(r'C:\datasets\vgg2\n000149\0014_01.jpg')
-    n = get_test_image(r'C:\datasets\vgg2\n000009\0011_01.jpg')
-    a = res_net.predict(a)
-    p = res_net.predict(p)
-    n = res_net.predict(n)
-    p_dist = np.linalg.norm(a - p)
-    n_dist = np.linalg.norm(a - n)
-    print(p_dist)
-    print(n_dist)
+        current_folder_labels = [current_label] * current_folder_files_count
+        all_labels.extend(current_folder_labels)
 
-    res_net.load_weights('resnet_weights.h5', by_name=True)
-    a = get_test_image(r'C:\datasets\vgg2\n000149\0003_01.jpg')
-    p = get_test_image(r'C:\datasets\vgg2\n000149\0014_01.jpg')
-    n = get_test_image(r'C:\datasets\vgg2\n000009\0011_01.jpg')
-    a = res_net.predict(a)
-    p = res_net.predict(p)
-    n = res_net.predict(n)
-    p_dist = np.linalg.norm(a - p)
-    n_dist = np.linalg.norm(a - n)
-    print(p_dist)
-    print(n_dist)
-
-
-def test_distance():
-    d, l = get_data(r'C:\datasets\test')
-    a, p, n = create_triplet(d, l)
-    res_net = create_resnet()
-
-    res = res_net.predict(a)
-    res2 = res_net.predict(p)
-    res3 = res_net.predict(n)
-    p_dist = np.linalg.norm(res - res2, axis=1)
-    n_dist = np.linalg.norm(res - res3, axis=1)
-    print(p_dist.mean())
-    print(n_dist.mean())
-
-    res_net.load_weights('resnet_weights_full.h5', by_name=True)
-
-    res = res_net.predict(a)
-    res2 = res_net.predict(p)
-    res3 = res_net.predict(n)
-    p_dist = np.linalg.norm(res - res2, axis=1)
-    n_dist = np.linalg.norm(res - res3, axis=1)
-    print(p_dist.mean())
-    print(n_dist.mean())
+        files_count += current_folder_files_count
+        all_files.extend(files)
+        current_label += 1
+    return all_files, all_labels
 
 
 if __name__ == '__main__':
     train_resnet()
-    # test_reznet()
-    # test_distance()
