ac11284c35483bc976fa7c5628764018714d26cc
zabik <zaaabik@gmail.com>
2019-03-08 23:37:08 +0300

    добавил новую текстуру для очков

--- data-augmentation/main.py
+++ data-augmentation/main.py
@@ -3,7 +3,9 @@ import os
 
 import cv2
 import dlib
+from PIL import Image
 from imutils import face_utils
+from imutils.face_utils import FACIAL_LANDMARKS_68_IDXS
 
 parser = optparse.OptionParser()
 parser.add_option("-f", "--folder")
@@ -26,22 +28,42 @@ def main():
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         rects = detector(gray, 0)
         for (i, rect) in enumerate(rects):
-            # Make the prediction and transfom it to numpy array
             shape = predictor(gray, rect)
             shape = face_utils.shape_to_np(shape)
-            left_eye = shape[36], shape[39]
-            right_eye = shape[42], shape[45]
-            print(left_eye[0])
-            print(left_eye[0])
-            cv2.circle(image, tuple(left_eye[0]), 2, (0, 255, 0), -1)
-            cv2.circle(image, tuple(left_eye[1]), 2, (0, 255, 0), -1)
-            cv2.circle(image, tuple(right_eye[0]), 2, (0, 255, 0), -1)
-            cv2.circle(image, tuple(right_eye[1]), 2, (0, 255, 0), -1)
 
-        # Show the image
-        cv2.imshow("Output", image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+            (lStart, lEnd) = FACIAL_LANDMARKS_68_IDXS["left_eye"]
+            (rStart, rEnd) = FACIAL_LANDMARKS_68_IDXS["right_eye"]
+
+            left_eye_pts = shape[lStart:lEnd]
+            right_eye_pts = shape[rStart:rEnd]
+
+            left_eye_center = left_eye_pts.mean(axis=0).astype("int")
+            right_eye_center = right_eye_pts.mean(axis=0).astype("int")
+
+            left_eye_end = shape[rStart]
+            right_eye_start = shape[lEnd - 3]
+
+            cv2.circle(image, tuple(left_eye_center), 2, (0, 255, 0), -1)
+            cv2.circle(image, tuple(right_eye_center), 2, (0, 255, 0), -1)
+
+            cv2.circle(image, tuple(right_eye_start), 2, (0, 255, 0), -1)
+            cv2.circle(image, tuple(left_eye_end), 2, (0, 255, 0), -1)
+
+            glasses = Image.open('filters/glasses3.png')
+            glasses_width = left_eye_center[0] - right_eye_center[0]
+
+            glasses_width = int((glasses_width / 2.5) * 5)
+            glasses_height = glasses_width // 2
+            glasses = glasses.resize((glasses_width, glasses_height))
+
+            face = Image.fromarray(image[:, :, ::-1])
+            glasses_offset = right_eye_center[0] - int(glasses_width / 4.5), left_eye_center[1] - glasses_height // 2
+            face.paste(glasses, glasses_offset, glasses)
+            face.show()
+            # Show the image
+        # cv2.imshow("Output", image)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
