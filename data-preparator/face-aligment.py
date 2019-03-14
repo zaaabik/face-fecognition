1c8179a6c082007bd6bc46d98fafe79e6d08efca
zabik <zaaabik@gmail.com>
2019-03-14 23:58:40 +0300

    fixed bad logging

--- data-preparator/face-aligment.py
+++ data-preparator/face-aligment.py
@@ -29,23 +29,22 @@ def main():
             continue
         print(idx)
         files_path = path + os.path.sep + folder
-        try:
-            files_path = path + os.path.sep + folder
-            files = os.listdir(files_path)
-            res_folder = out + os.path.sep + folder
-            if not os.path.isdir(res_folder):
-                os.mkdir(res_folder)
-            for file in files:
-                file_path = files_path + os.path.sep + file
+        files = os.listdir(files_path)
+        res_folder = out + os.path.sep + folder
+        if not os.path.isdir(res_folder):
+            os.mkdir(res_folder)
+        for file in files:
+            file_path = files_path + os.path.sep + file
+            try:
                 img = cv2.imread(file_path)
                 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
                 rects = detector(img_gray, 0)
                 if len(rects) == 1:
                     aligned_face = face_aligner.align(img, img_gray, rects[0])
                     cv2.imwrite(res_folder + os.path.sep + file, aligned_face)
-        except Exception as e:
-            print(files_path)
-            print(str(e))
+            except Exception as e:
+                print(file_path)
+                print(str(e))
 
 
 if __name__ == '__main__':
