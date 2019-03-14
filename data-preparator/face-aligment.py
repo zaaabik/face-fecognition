6823c90607d019447c04249feffdfd92060d23a9
zabik <zaaabik@gmail.com>
2019-03-14 23:53:12 +0300

    added argument for skip images + logging exception

--- data-preparator/face-aligment.py
+++ data-preparator/face-aligment.py
@@ -8,9 +8,11 @@ from imutils.face_utils import FaceAligner
 parser = optparse.OptionParser()
 parser.add_option('--path')
 parser.add_option('--out')
+parser.add_option('--skip')
 (options, args) = parser.parse_args()
 path = options.path
 out = options.out
+skip = options.skip
 
 
 def main():
@@ -23,20 +25,27 @@ def main():
     if not os.path.isdir(out):
         os.mkdir(out)
     for idx, folder in enumerate(folders):
+        if idx < skip:
+            continue
         print(idx)
         files_path = path + os.path.sep + folder
-        files = os.listdir(files_path)
-        res_folder = out + os.path.sep + folder
-        if not os.path.isdir(res_folder):
-            os.mkdir(res_folder)
-        for file in files:
-            file_path = files_path + os.path.sep + file
-            img = cv2.imread(file_path)
-            img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            rects = detector(img_gray, 0)
-            if len(rects) == 1:
-                aligned_face = face_aligner.align(img, img_gray, rects[0])
-                cv2.imwrite(res_folder + os.path.sep + file, aligned_face)
+        try:
+            files_path = path + os.path.sep + folder
+            files = os.listdir(files_path)
+            res_folder = out + os.path.sep + folder
+            if not os.path.isdir(res_folder):
+                os.mkdir(res_folder)
+            for file in files:
+                file_path = files_path + os.path.sep + file
+                img = cv2.imread(file_path)
+                img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                rects = detector(img_gray, 0)
+                if len(rects) == 1:
+                    aligned_face = face_aligner.align(img, img_gray, rects[0])
+                    cv2.imwrite(res_folder + os.path.sep + file, aligned_face)
+        except Exception as e:
+            print(files_path)
+            print(str(e))
 
 
 if __name__ == '__main__':
