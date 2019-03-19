d8902586a645641a7984361a3a6a62c1c2f76cd9
zabik <zaaabik@gmail.com>
2019-03-19 18:34:00 +0300

    added align function

--- validators/megaface.py
+++ validators/megaface.py
@@ -3,6 +3,7 @@ import json
 import optparse
 import os
 
+import cv2
 import numpy as np
 from skimage.io import imread
 from skimage.transform import resize
@@ -51,15 +52,43 @@ def main():
             save_mat(out_path, embedding[idx])
 
 
+def align():
+    with open(images_paths) as file:
+        feature_file = json.load(file)
+        paths = feature_file['path']
+        images = []
+        for path in paths:
+            image_path = os.path.join(base_path, path)
+            image = cv2.imread(image_path)
+            try:
+                image = face_align(image)
+                images.append(image)
+            except:
+                images.append(image)
+                print(f'no face in {image_path}')
+
+            out_path = os.path.join(out, path)
+            head, _ = os.path.split(out_path)
+            is_exists = os.path.exists(head)
+            if not is_exists:
+                mkdir_p(head)
+            cv2.imwrite(out_path, image)
+
+
 if __name__ == '__main__':
     parser = optparse.OptionParser()
     parser.add_option('--images_paths', type='string')
     parser.add_option('--base_path', type='string')
     parser.add_option('--weights', type='string')
     parser.add_option('--out', type='string')
+    parser.add_option('--mode', type='string', default='full')
     options, _ = parser.parse_args()
     images_paths = options.images_paths
     base_path = options.base_path
     weights = options.weights
     out = options.out
-    main()
+    mode = options.mode
+    if mode == 'full':
+        main()
+    elif mode == 'align':
+        align()
