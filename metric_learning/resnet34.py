b7acd089dfed17b2b58a59a5e57c0012e521b4c5
zabik <zaaabik@gmail.com>
2019-05-12 00:40:47 +0300

    delete comments

--- metric_learning/resnet34.py
+++ metric_learning/resnet34.py
@@ -58,43 +58,16 @@ def resnet_layer(inputs,
 
 
 def resnet_v2(input_shape, depth, out_size=128):
-    """ResNet Version 2 Model builder [b]
-
-    Stacks of (1 x 1)-(3 x 3)-(1 x 1) BN-ReLU-Conv2D or also known as
-    bottleneck layer
-    First shortcut connection per layer is 1 x 1 Conv2D.
-    Second and onwards shortcut connection is identity.
-    At the beginning of each stage, the feature map size is halved (downsampled)
-    by a convolutional layer with strides=2, while the number of filter maps is
-    doubled. Within each stage, the layers have the same number filters and the
-    same filter map sizes.
-    Features maps sizes:
-    conv1  : 32x32,  16
-    stage 0: 32x32,  64
-    stage 1: 16x16, 128
-    stage 2:  8x8,  256
-
-    # Arguments
-        input_shape (tensor): shape of input image tensor
-        depth (int): number of core convolutional layers
-        num_classes (int): number of classes (CIFAR10 has 10)
-
-    # Returns
-        model (Model): Keras model instance
-    """
     if (depth - 2) % 9 != 0:
         raise ValueError('depth should be 9n+2 (eg 56 or 110 in [b])')
-    # Start model definition.
     num_filters_in = 16
     num_res_blocks = int((depth - 2) / 9)
 
     inputs = Input(shape=input_shape)
-    # v2 performs Conv2D with BN-ReLU on input before splitting into 2 paths
     x = resnet_layer(inputs=inputs,
                      num_filters=num_filters_in,
                      conv_first=True)
 
-    # Instantiate the stack of residual units
     for stage in range(3):
         for res_block in range(num_res_blocks):
             activation = 'relu'
@@ -102,15 +75,14 @@ def resnet_v2(input_shape, depth, out_size=128):
             strides = 1
             if stage == 0:
                 num_filters_out = num_filters_in * 4
-                if res_block == 0:  # first layer and first stage
+                if res_block == 0:
                     activation = None
                     batch_normalization = False
             else:
                 num_filters_out = num_filters_in * 2
-                if res_block == 0:  # first layer but not first stage
-                    strides = 2  # downsample
+                if res_block == 0:
+                    strides = 2
 
-            # bottleneck residual unit
             y = resnet_layer(inputs=x,
                              num_filters=num_filters_in,
                              kernel_size=1,
@@ -126,8 +98,6 @@ def resnet_v2(input_shape, depth, out_size=128):
                              kernel_size=1,
                              conv_first=False)
             if res_block == 0:
-                # linear projection residual shortcut connection to match
-                # changed dims
                 x = resnet_layer(inputs=x,
                                  num_filters=num_filters_out,
                                  kernel_size=1,
@@ -138,14 +108,12 @@ def resnet_v2(input_shape, depth, out_size=128):
 
         num_filters_in = num_filters_out
 
-    # Add classifier on top.
-    # v2 has BN-ReLU before Pooling
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
     x = AveragePooling2D(pool_size=8)(x)
     y = Flatten()(x)
     outputs = Dense(out_size)(y)
-    # Instantiate model.
+    
     model = Model(inputs=inputs, outputs=outputs)
     return model
 
@@ -336,7 +304,6 @@ class Resnet34:
             prev = Activation('relu')(prev)
             prev = BatchNormalization()(prev)
             prev = MaxPool2D(pool_size=(3, 3), strides=(2, 2))(prev)
-
             prev = self.level4(prev)
             prev = self.level3(prev)
             prev = self.level2(prev)
