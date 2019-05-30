78c374535673d0fb91c8f6e96a1c05bf54491413
zabik <zaaabik@gmail.com>
2019-05-31 00:27:51 +0300

    multiprocessing face-aligment

--- data-preparator/face-aligment.py
+++ data-preparator/face-aligment.py
@@ -1,6 +1,7 @@
-import errno
+import json
 import optparse
 import os
+from multiprocessing.dummy import Pool as ThreadPool
 
 import cv2
 import dlib
@@ -11,18 +12,22 @@ from helpers.helpers import mkdir_p
 parser = optparse.OptionParser()
 parser.add_option('--path')
 parser.add_option('--out')
+parser.add_option('--cpu', type='int', default=4)
+parser.add_option('--file', type='string')
 parser.add_option('--skip', type='int', default=0)
 (options, args) = parser.parse_args()
 path = options.path
 out = options.out
+cpu = options.cpu
+file = options.file
 skip = options.skip or 0
 
+predictor = dlib.shape_predictor("../shape_predictor_68_face_landmarks.dat")
+face_aligner = FaceAligner(predictor=predictor, desiredLeftEye=(0.315, 0.315), desiredFaceWidth=128)
+detector = dlib.get_frontal_face_detector()
 
-def main():
-    predictor = dlib.shape_predictor("../shape_predictor_68_face_landmarks.dat")
-    face_aligner = FaceAligner(predictor=predictor, desiredLeftEye=(0.315, 0.315), desiredFaceWidth=128)
-    detector = dlib.get_frontal_face_detector()
 
+def align_folder():
     folders = os.listdir(path)
 
     if not os.path.isdir(out):
@@ -51,5 +56,36 @@ def main():
                 print(str(e))
 
 
+def align_from_file():
+    with open(file) as images_file:
+        feature_file = json.load(images_file)
+        paths = feature_file['path']
+
+    pool = ThreadPool(cpu)
+    pool.map(align_image, paths)
+    pool.close()
+    pool.join()
+
+
+def align_image(file_path):
+    output_directory = os.path.dirname(file_path)
+    output_directory = os.path.join(out, output_directory)
+    output_file = os.path.join(out, file_path)
+    mkdir_p(output_directory)
+    try:
+        img = cv2.imread(os.path.join(path, file_path))
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        rects = detector(img_gray, 0)
+        if len(rects) == 1:
+            aligned_face = face_aligner.align(img, img_gray, rects[0])
+            cv2.imwrite(output_file, aligned_face)
+    except Exception as e:
+        print(file_path)
+        print(str(e))
+
+
 if __name__ == '__main__':
-    main()
+    if file is not None:
+        align_from_file()
+    else:
+        align_folder()
