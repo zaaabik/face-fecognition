99588416f242f8878e00c6e2c770577d7b713ba8
zabik <zaaabik@gmail.com>
2019-05-31 18:29:55 +0300

    script for download dataset

---
+++ scripts/download-datasets.py
@@ -0,0 +1,20 @@
+import optparse
+import os
+
+parser = optparse.OptionParser()
+parser.add_option('--out', type='sting', default='/home/fr/dataset')
+(options, args) = parser.parse_args()
+out = options.out
+
+http_user = 'zhangdo@uestc.edu.cn'
+http_password = '1O|)8f+%Z8'
+
+
+def main():
+    wget = f'wget http://megaface.cs.washington.edu/dataset/download/content/MegaFace_dataset.tar.gz -P {out} ' \
+           f'--http-user={http_user} --http-password={http_password}'
+    os.system(wget)
+
+
+if __name__ == '__main__':
+    main()
