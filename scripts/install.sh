c189fc2cc3fd7d283d09f2bb07e7ca5c012ffd74
zabik <zaaabik@gmail.com>
2019-05-31 18:38:44 +0300

    added scripts for python install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,8 @@
+sudo add-apt-repository ppa:jonathonf/python-3.6
+sudo apt-get update
+sudo apt-get install python3.6
+
+
 pip install virtualenv
 virtualenv face-recognition
 source face-recognition/bin/activate
